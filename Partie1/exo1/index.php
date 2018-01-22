66c8f82b2912b7dd1eb1adab1baeb4bdf1b4c165
Thomas Szymanski <thomas.szymanski15@gmail.com>
2018-01-22 09:44:05 +0100

    First commit

---
+++ Partie1/exo1/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Exercice 1 - PDO</title>
+        <meta charset="utf-8" />
+    </head> 
+    <body>
+            
+    </body>
+</html>
